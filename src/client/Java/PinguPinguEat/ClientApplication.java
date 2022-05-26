8267c562792b8ce53b7f261b6cc883288d469f6c
Maxi Wimmer <mc.w1mm3r@gmail.com>
2022-05-26 18:44:24 +0200

    Application with fxml is working

--- src/client/Java/PinguPinguEat/ClientApplication.java
+++ src/client/Java/PinguPinguEat/ClientApplication.java
@@ -6,7 +6,9 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 public class ClientApplication extends Application {
     private ConfigurableApplicationContext applicationContext;
@@ -15,12 +17,12 @@ public class ClientApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
+        URL url = new File("src/client/resources/PinguPinguEat/ClientBase.fxml").toURI().toURL();
         this.stage =  primaryStage;
-        FXMLLoader fxmlLoader = new FXMLLoader(ClientApplication.class.getResource("ClientBase.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(),320,240);
+        FXMLLoader fxmlLoader = new FXMLLoader(url);
+        Scene scene = new Scene(fxmlLoader.load(),1080,720);
         stage.setTitle("Build 0.0.0.1");
         stage.setScene(scene);
         stage.show();
     }
-
 }
