750ca2ea2fa15d2dff93f3b3e4568c2afa72c605
Maxi Wimmer <mc.w1mm3r@gmail.com>
2022-05-26 21:33:32 +0200

    Deleted old HelloController.java Added Basic functionalities -> Controller funktioniert usw.

--- src/main/java/PinguPinguEat/restaurants/PriceCategory.java
+++ src/main/java/PinguPinguEat/restaurants/PriceCategory.java
@@ -1,8 +1,4 @@
-<<<<<<< HEAD:src/main/java/restaurants/PriceCategory.java
-package Restaurants;
-=======
 package PinguPinguEat.restaurants;
->>>>>>> Another_New_Structure:src/main/java/PinguPinguEat/restaurants/PriceCategory.java
 
 public enum PriceCategory {
     INEXPENSIVE, MODERATE, EXPENSIVE, VERY_EXPENSIVE
