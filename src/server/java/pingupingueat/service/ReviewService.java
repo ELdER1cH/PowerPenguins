58810d6d1c93cc782500502ad18c6725f21b73b3
Ellie033 <2303121765a@gmail.com>
2022-06-03 18:31:48 +0200

    Review Endpoint try 01 , by Ellie

---
+++ src/server/java/pingupingueat/service/ReviewService.java
@@ -0,0 +1,38 @@
+package pingupingueat.service;
+
+import org.springframework.stereotype.Service;
+import pingupingueat.user.Review;
+
+import java.util.List;
+
+@Service
+public class ReviewService {
+    private final List<Review> reviews;
+
+    public ReviewService(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    public Review postReview(Review review) {
+        boolean isSuccessful = reviews.add(review);
+//do not allow an empty review
+        if (review.getComment() == null) {
+            isSuccessful = false;
+        }
+
+        if (isSuccessful) {
+            return review;
+        } else {
+            return null;
+        }
+    }
+
+    public boolean deleteReview(Review review) {
+        return reviews.remove(review);
+    }
+
+    public List<Review> getAllReviews() {
+// Maybe add some sorting options in the future
+        return this.reviews;
+    }
+}
