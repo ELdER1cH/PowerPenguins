5978834a56dc9f15ad5156c6fa0a7ce2388fea37
Maxi Wimmer <mc.w1mm3r@gmail.com>
2022-06-03 20:04:57 +0200

    updated UI
    Signed-off-by: Maxi Wimmer <mc.w1mm3r@gmail.com>

--- src/client/java/pinguPinguEat/view/ClientBase.java
+++ src/client/java/pinguPinguEat/view/ClientBase.java
@@ -1,20 +1,65 @@
+/**
+ * Sample Skeleton for 'ClientBase.fxml' Controller Class
+ */
+
 package pinguPinguEat.view;
 
-
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import pinguPinguEat.restaurants.Restaurant;
 
-
-//Controller für die Client Base fxml Datei -> die ganze Logic von der Anzeige wird hier gemacht
 public class ClientBase {
-    @FXML
-    private Label text;
+
+    @FXML // fx:id="HelloSign"
+    private Label HelloSign; // Value injected by FXMLLoader
+
+    @FXML // fx:id="loginButton"
+    private Button loginButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="passWordField"
+    private TextField passWordField; // Value injected by FXMLLoader
+
+    @FXML // fx:id="registerButton"
+    private Button registerButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="restaurantList"
+    private ListView<Restaurant> restaurantList; // Value injected by FXMLLoader
+
+    @FXML // fx:id="searchButton"
+    private Button searchButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="searchField"
+    private TextField searchField; // Value injected by FXMLLoader
+
+    @FXML // fx:id="type1"
+    private ImageView type1; // Value injected by FXMLLoader
+
+    @FXML // fx:id="userNameField"
+    private TextField userNameField; // Value injected by FXMLLoader
 
     @FXML
-    private Label HelloSign;
+    void loginAction(ActionEvent event) {
 
-    @FXML
-    protected void onHelloButtonClick(){
-        HelloSign.setText("Have Fun!");
     }
+
+    @FXML
+    void onHelloButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void registerAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchAction(ActionEvent event) {
+
+    }
+
 }
