916909cd1a48bd4d79feff03d73bc413b072687c
Ellie033 <2303121765a@gmail.com>
2022-06-10 19:48:09 +0200

    Review Client Endpoint try 5 by Ellie

--- src/main/java/pinguPinguEat/user/User.java
+++ src/main/java/pinguPinguEat/user/User.java
@@ -5,7 +5,6 @@ import pinguPinguEat.reservationModel.Reservation;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-
 public class User {
     private final String name;
     private final List<Reservation> reservations;
