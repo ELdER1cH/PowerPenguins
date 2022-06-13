d0dbc0ce25f2add6764a4ee5a77b93fbd88bd348
rosa <ge95gaf@mytum.de>
2022-06-13 20:17:53 +0200

    noch einige Fehler, zu ueberarbeiten

---
+++ src/client/java/pinguPinguEat/controller/RestaurantController.java
@@ -0,0 +1,115 @@
+package pinguPinguEat.controller;
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+import pinguPinguEat.reservationModel.Reservation;
+import pinguPinguEat.reservationModel.Table;
+import pinguPinguEat.reservationModel.TimeSlot;
+import pinguPinguEat.restaurants.Restaurant;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+public class RestaurantController {
+    private final WebClient webClient;
+    private final HashMap<UUID, Restaurant> restaurantHashMap;
+
+    public RestaurantController() {
+        this.webClient = WebClient.builder()
+                .baseUrl("http://localhost:8080/")
+                .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+        this.restaurantHashMap = new HashMap<>();
+    }
+
+    public void getAllRestaurants(Consumer<HashMap<UUID, Restaurant>> restaurantConsumer) {
+        webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/restaurants")
+                        .build())
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<List<Restaurant>>() {
+                })
+                .onErrorStop()
+                .subscribe(newRestaurants -> {
+                    restaurantHashMap.clear();
+                    restaurantHashMap.putAll(newRestaurants); // falsch, muss HashMap sein
+                    restaurantConsumer.accept(restaurantHashMap);
+                });
+    }
+
+    public void getRestaurant(Consumer<List<Restaurant>> restaurantConsumer, UUID restaurantID) {
+//        boolean isRestaurant = false;
+//        int i;
+//        for(i = 0; i < restaurantList.size(); i++){
+//            if(restaurantList.get(i).getReservationID().equals(restaurantID)){
+//                isRestaurant = true;
+//                break;
+//            }
+//        }
+//        // Restaurant mit dieser ID existiert nicht
+//        if(!isRestaurant){ return;}
+
+        webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/restaurants")
+                        .build())
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<List<Restaurant>>() {
+                })
+                .onErrorStop()
+                .subscribe(newRestaurants -> {
+                    restaurantHashMap.clear();
+                    restaurantHashMap.putAll(newRestaurants); // muss HashMap sein, falsch
+                    restaurantConsumer.accept(restaurantHashMap.get(restaurantID).toList ?);
+
+                });
+    }
+
+    public void getAllTables(Consumer<List<Table>> tableConsumer, UUID restaurantID) {
+        webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/restaurants/" + restaurantID)
+                        .build())
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<List<Table>>() {
+                })
+                .onErrorStop()
+                .subscribe(newRestaurants -> {
+                    restaurantHashMap.clear();
+                    restaurantHashMap.putAll(newRestaurants); // muss HashMap sein, falsch
+                    tableConsumer.accept(restaurantHashMap.get(restaurantID).getReservationSystem().getTables());
+                });
+    }
+
+    public void getFreeTables(Consumer<List<Table>> tableConsumer, UUID restaurantID, TimeSlot timeSlot) {
+        List<Table> allTables = restaurantHashMap.get(restaurantID).getReservationSystem().getTables();
+        List<Reservation> reservations = ((Restaurant) restaurantHashMap.get(restaurantID)).getReservationSystem().getReservations().get(timeSlot);
+        for (Reservation reservation : reservations) {
+            if (allTables.contains(reservation.getTable())) {
+                reservation = null;
+            }
+
+        }
+
+        webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/restaurants/" + restaurantID)
+                        .build())
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<List<Table>>() {
+                })
+                .onErrorStop()
+                .subscribe(newRestaurants -> {
+                    restaurantHashMap.clear();
+                    restaurantHashMap.putAll(newRestaurants); // muss HashMap sein, falsch
+                    tableConsumer.accept(allTables.stream().filter(x -> x != null).collect(Collectors.toList()));
+                });
+    }
+}
