fc4510ce9fb166cd86f0ec82b11be1f62c9605d8
CamelotMacaw <91760131+CamelotMacaw@users.noreply.github.com>
2022-07-11 18:43:01 +0200

    added buttons & cancel

--- src/client/java/pinguPinguEat/view/ReservationGroupController.java
+++ src/client/java/pinguPinguEat/view/ReservationGroupController.java
@@ -2,12 +2,11 @@ package pinguPinguEat.view;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListView;
-import javafx.scene.input.MouseEvent;
 import pinguPinguEat.logic.MyReservationLogic;
 import pinguPinguEat.reservationModel.Reservation;
 
@@ -17,6 +16,10 @@ public class ReservationGroupController {
     private ObservableList<Reservation> userReservations = FXCollections.observableArrayList();
     @FXML // fx:id="reservationList"
     private ListView<Reservation> reservationList; // Value injected by FXMLLoader
+    @FXML // fx:id="confirmButton"
+    private Button confirmButton; // Value injected by FXMLLoader
+    @FXML // fx:id="cancelButton"
+    private Button cancelButton; // Value injected by FXMLLoader
 
     public ReservationGroupController() {
         userReservations.addAll(MyReservationLogic.getAllReservations());
@@ -24,38 +27,45 @@ public class ReservationGroupController {
 
     public void loadList() {
         reservationList.setItems(userReservations);
-        setUpMouseClickActions();
     }
 
-    private void setUpMouseClickActions() {
-        reservationList.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                Reservation thisReservation = reservationList.getSelectionModel().getSelectedItem();
-                // this way the popup doesn't appear if it's already confirmed
-                if (!thisReservation.isConfirmable()) {
-                    return;
-                }
 
-                Alert confirm = new Alert(Alert.AlertType.CONFIRMATION);
-                confirm.setTitle("Confirmation");
-                confirm.setHeaderText(null);
-                confirm.setContentText("Confirm this reservation?");
-                Optional<ButtonType> answer = confirm.showAndWait();
-                if (answer.get() == ButtonType.OK) {
-                    thisReservation.confirm();
-                    System.out.println(thisReservation.isConfirmed());
-                    update(userReservations, thisReservation);
-                }
-                System.out.println("name: " + reservationList.getSelectionModel().getSelectedItems().toString());
-            }
-        });
-    }
 
     private void update(ObservableList<Reservation> reservations, Reservation updatedReservation) {
         // this may seem overcomplicated but this was necessary for some reason
         reservations.removeIf(x -> x.getReservationId().equals(updatedReservation.getReservationId()));
         reservations.add(updatedReservation);
         reservationList.setItems(reservations);
+    }
+
+    public void confirmButtonPressed() {
+        Reservation thisReservation = reservationList.getSelectionModel().getSelectedItem();
+        if (thisReservation == null) {
+            return;
+        }
+        // this way the popup doesn't appear if it's already confirmed
+        if (!thisReservation.isConfirmable()) {
+            return;
+        }
+
+        Alert confirm = new Alert(Alert.AlertType.CONFIRMATION);
+        confirm.setTitle("Confirmation");
+        confirm.setHeaderText(null);
+        confirm.setContentText("Confirm this reservation?");
+        Optional<ButtonType> answer = confirm.showAndWait();
+        if (answer.get() == ButtonType.OK) {
+            thisReservation.confirm();
+            System.out.println(thisReservation.isConfirmed());
+            update(userReservations, thisReservation);
+        }
+    }
+
+    public void cancelButtonPressed() {
+        Reservation thisReservation = reservationList.getSelectionModel().getSelectedItem();
+        if (thisReservation == null) {
+            return;
+        }
+        userReservations.removeIf(x -> x.getReservationId().equals(thisReservation.getReservationId()));
+        reservationList.setItems(userReservations);
     }
 }
